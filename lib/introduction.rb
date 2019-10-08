ef1d35cfe9c507a501126cea5806279c3441b045
Eric Manning <emanningmusic@yahoo.com>
2019-10-08 04:19:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,11 +1,12 @@
+name = "Josh"
 def introduction(name)
   puts "Hi,my name is #(name)!"
 end
 
-introduction("Josh")
+name = "Dan"
+language = "Ember.js"
 
 def introduction_with_language(name,language)
   puts "Hi, my name is #(name) and i am learing to program in #(language)."
 end
 
-introduction_with_language("Dan","Ember.js")
